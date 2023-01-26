490ca245de6114a05af8c2e1d5945c8fe8545c35
Johanna <johannagd96@gmail.com>
2023-01-26 18:04:40 +0100

    Initial commit

---
+++ src/main/java/com/example/klausrslmjohanna/KlausrSlmJohannaApplication.java
@@ -0,0 +1,13 @@
+package com.example.klausrslmjohanna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlausrSlmJohannaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlausrSlmJohannaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/klausrslmjohanna/KlausrSlmJohannaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.klausrslmjohanna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlausrSlmJohannaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
